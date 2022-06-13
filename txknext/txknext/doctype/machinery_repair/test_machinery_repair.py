5260742f2d7e6f9eb389af9d9cc7d847fedaab42
Christos Chochlidakis <chdcomputers@gmail.com>
2022-06-13 23:03:27 +0300

    machinery repairs

---
+++ txknext/txknext/doctype/machinery_repair/test_machinery_repair.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2022, ChD Computers and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class TestMachineryRepair(FrappeTestCase):
+	pass
